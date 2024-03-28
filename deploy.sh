85c63787d63eba2d32aa8b66869886233e90944f
spacewall <godovav@oiate.ru>
2024-03-28 17:55:57 +0300

    Finish deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,7 @@
 cd /home/spacewall/ci_homework
+git pull origin master
+source venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+sudo systemctl restart gunicorn
